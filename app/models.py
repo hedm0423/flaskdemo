8c588498a09546c08e540052e6f890704c9b33e6
adolf <hedm0423@163.com>
2017-02-08 15:06:38 +0800

    fix the user profiles

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app,request
+import hashlib
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -51,6 +53,12 @@ class User(UserMixin,db.Model):
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default=False)
+	name = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	about_me = db.Column(db.Text())
+	member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+	last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+	avatar_hash = db.Column(db.String(32))
 
 	def __init__(self,**kwargs):
 		super(User,self).__init__(**kwargs)
@@ -59,6 +67,20 @@ class User(UserMixin,db.Model):
 				self.role = Role.query.filter_by(permissions=0xff).first()
 			if self.role is None:
 				self.role = Role.query.filter_by(default=True).first()
+			if self.email is not None and self.avatar_hash is None:
+				self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+
+	def gravatar(self,size=100,default='identicon',rating='g'):
+		if request.is_secure:
+			url = 'https://secure.gravatar.com/avatar'
+		else:
+			url = 'http://www.gravatar.com/avatar'
+		hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+		return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
+    
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
 
 	@property
 	def password(self):
@@ -84,6 +106,43 @@ class User(UserMixin,db.Model):
 		if data.get('confirm') != self.id:
 			return False
 		self.confirmed = True
+		db.session.add(self)
+		return True
+
+	def generate_reset_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'reset': self.id})
+
+	def reset_password(self, token, new_password):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('reset') != self.id:
+			return False
+		self.password = new_password
+		db.session.add(self)
+		return True
+
+	def generate_email_change_token(self, new_email, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+	def change_email(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('change_email') != self.id:
+			return False
+		new_email = data.get('new_email')
+		if new_email is None:
+			return False
+		if self.query.filter_by(email=new_email).first() is not None:
+			return False
+		self.email = new_email
 		db.session.add(self)
 		return True
 
